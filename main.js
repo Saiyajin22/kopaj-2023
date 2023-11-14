b364a816e789f1324ba2c3b0aa026b34643cfe72
vargai-novin <vargaizsak412@gmail.com>
2023-11-14 22:28:35 +0100

    dassda

--- main.js
+++ main.js
@@ -219,6 +219,7 @@ app.post('/level1/task3', function (req, res) {
          if(rows[i].includes(intactBunnyPattern[0])){
             if (rows[i+1].includes(intactBunnyPattern[1]) && rows[i+1].indexOf(intactBunnyPattern[1][0]) ===rows[i].indexOf(intactBunnyPattern[0][0])){
                if(rows[i+2].includes(intactBunnyPattern[2]) && rows[i+2].indexOf(intactBunnyPattern[2][0]) === rows[i+1].indexOf(intactBunnyPattern[1][0])) {
+                  console.log("asdasda"+ i + " " + rows[i+1].indexOf(intactBunnyPattern[1][2]))
                   res.send(i + " " + rows[i+1].indexOf(intactBunnyPattern[1][2]));
                }
             }
@@ -247,7 +248,7 @@ app.post('/level1/task3', function (req, res) {
    //    return true;
    //  }
 
-    const result = findIntactBunnyNose(req.body);
+    findIntactBunnyNose(req.body);
 })
 
 app.post('/level2/task1', function (req, res) {
