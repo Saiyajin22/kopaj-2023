25d5099920428dfd2d26435788a51fc88c44cf19
vargai-novin <vargaizsak412@gmail.com>
2023-11-14 20:15:10 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# kopaj-2023
-Kopaj 2023 progverseny
+1. Install node and npm: if you don't have https://nodejs.org/en/download/
+2. Install express: `npm install`
+3. Run the service: `npm start`
+4. Make sure your service is available form local network.
+
+Express documentation: https://expressjs.com/
+
---
+++ main.js
@@ -0,0 +1,35 @@
+var bodyParser = require('body-parser')
+var express = require('express');
+
+var app = express();
+
+app.use(bodyParser.json({ type: 'application/json' }))
+app.use(bodyParser.text({ type: 'text/plain' }))
+
+/**
+ * This simple Express App can serve as an example for you. Take a look at the endpoint definitions, you will need to create similar ones!
+ */
+
+
+app.post('/ground/task1', function (req, res) {
+   console.log("Headers: " + JSON.stringify(req.headers))
+   console.log("Body: " + req.body)
+   res.status(201);
+   res.send('Hello');
+})
+
+app.post('/level1/task1', function (req, res) {
+   res.send('Hello');
+})
+
+app.post('/level2/task1', function (req, res) {
+   res.send("Hello");
+})
+
+app.post('/level3/task1', function (req, res) {
+   res.send('Hello');
+})
+
+var server = app.listen(1234, '0.0.0.0', function () {
+   console.log("App listening at http://%s:%s", server.address().address, server.address().port)
+})
